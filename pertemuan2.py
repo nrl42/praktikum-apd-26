8fc07bd5b71e99aed35f9dc058a77515ad2f64c4
NRL <nrlazizah47@gmail.com>
2026-09-17 16:19:12 +0800

    jenis tipe data

---
+++ pertemuan2.py
@@ -0,0 +1,53 @@
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+
+# nama = "Larry"
+# hobi = "ngoding"
+# print(nama)
+# print(hobi)
+
+# print('''Halo saya Rendy
+# Saya suka belajar coding
+# Hari ini saya belajar bahasa pemrograman Python''')
+
+# teks = "Rahmat Flowchart"
+# #menampilkan index ke-3
+# print(teks[3])
+# #menampilkan index ke-3 hingga ke-6
+# print(teks[3:6])
+# #menampilkan seluruhnya, tetapi dimulai dari index ke-7
+# print(teks[7:])
+# #menampilkan hanya sampai index ke-6
+# print(teks[:6])
+
+# mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])
+
+# buku = {
+# 'judul' : 'Atomic Habits',
+# 'penulis' : 'James Clear',
+# 'halaman' : 320
+# }
+# ...
+# # Mengakses Value dalam Dictionary
+# print(buku['judul'])
+# print(buku['penulis'])
+# print(buku['halaman'])
+
+# fina = 30
+# difty = "20"
+# tambah = fina + int(difty)
+# print(tambah)
+
+# nama = input("Masukan nama anda: ")
+# print(nama)
+
+a = int(input("masukkan nilai a:"))
+b = int(input("masukkan nilai b:"))
+c = a * b
+print(c)
+
